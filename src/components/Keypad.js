196e72b671452dd890c2a347e8b0d109fd13144c
Thomas <thomasmckenna12gmail.com>
2021-11-21 19:48:59 -0500

    react-hooks-event-handling-lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+
+function KeyPad() {
+
+function prompt() {
+    console.log('Entering password...')
+}
+
+    return (
+        <div>
+            <input type="password" onChange={prompt}></input>
+        </div>
+    )
+}
+
+export default KeyPad
